57b6e7bae441958e31072176343513ad1cbfa64d
J-Appleg8 <84997748+J-Appleg8@users.noreply.github.com>
2022-05-30 15:03:31 -0700

    finished movie app

--- src/components/MoviesList.js
+++ src/components/MoviesList.js
@@ -1,16 +1,15 @@
 import React from 'react';
-
 import Movie from './Movie';
 import classes from './MoviesList.module.css';
 
-const MovieList = (props) => {
+const MovieList = props => {
   return (
     <ul className={classes['movies-list']}>
-      {props.movies.map((movie) => (
+      {props.movies.map(movie => (
         <Movie
           key={movie.id}
           title={movie.title}
-          releaseDate={movie.releaseDate}
+          releaseDate={movie.release}
           openingText={movie.openingText}
         />
       ))}
